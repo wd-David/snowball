ec47dc62ff48ed1e51c54202d420be99b674f0a8
AdrieneTZ <tznali17@gmail.com>
2022-10-26 21:04:50 +0800

    feat(back-end): add authenticate request middleware

---
+++ back-end/middleware/authReq.js
@@ -0,0 +1,15 @@
+const passport = require('../config/passport')
+
+function authenticateUser(req, res, next) {
+  const callbackFn = (error, user) => {
+    if (error || !user) {
+      return res.json({ message: 'Unauthorized request' })
+    }
+    req.user = user
+    next()
+  }
+
+  passport.authenticate('jwt', { session: false }, callbackFn)(req, res, next)
+}
+
+module.exports = authenticateUser
