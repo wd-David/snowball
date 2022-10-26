045cd9606c0600ab35a07060e38a03b9c9ef4822
AdrieneTZ <tznali17@gmail.com>
2022-10-26 10:40:35 +0800

    feat(back-emd): add route to get all categories

---
+++ back-end/controllers/category-controller.js
@@ -0,0 +1,19 @@
+const { Prisma, PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient({ log: ['query'] })
+
+const categoryController = {
+  // Get all categories
+  // URL: /categories
+  getCategories: async (req, res, next) => {
+    try {
+      const categories = await prisma.category.findMany()
+
+      res.json(categories)
+    } catch (error) {
+      next(error)
+    }
+  },
+}
+
+module.exports = categoryController
--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const users = require('./modules/users')
 const records = require('./modules/records')
+const categories = require('./modules/categories')
 
 router.use('/users', users)
 router.use('/records', records)
+router.use('/categories', categories)
 
 module.exports = router
