2d00b622bdee8e72c0550104867dac688d9e227b
AdrieneTZ <tznali17@gmail.com>
2022-10-26 21:44:53 +0800

    feat(back-end): add authenticate middleware on routes

--- back-end/routes/modules/users.js
+++ back-end/routes/modules/users.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../../config/passport')
 
 const userController = require('../../controllers/user-controller')
 
-router.post('/logIn', userController.logIn)
+router.post(
+  '/logIn',
+  passport.authenticate('local', { session: false }),
+  userController.logIn
+)
 router.post('/register', userController.register)
 
 module.exports = router
