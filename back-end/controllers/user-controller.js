771c2627a76f025c4321187487d8bedaef7e6237
AdrieneTZ <tznali17@gmail.com>
2022-10-30 14:04:15 +0800

    feat: add authentication process on log in route

--- back-end/controllers/user-controller.js
+++ back-end/controllers/user-controller.js
@@ -1,30 +1,39 @@
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
 const userController = {
-  // user log in
+  // User log in
+  // URL: /user/logIn
   logIn: async (req, res, next) => {
-    try {
-      // #swagger.tags = ['User']
-      const { email, password } = req.body
-      if (!email || !password) return res.json('missing email or password')
+    // Check if there are email and password in req.body
+    const { email, password } = req.body
+    if (!email || !password) return res.json(`missing user's email or password`)
 
+    try {
+      // Check if the current user has registered an account
       const user = await prisma.user.findUnique({
         where: {
           email: email,
         },
       })
-      user ? user : res.json('incorrect email or unregistered email')
+      user
+        ? user
+        : res.json('Incorrect email address or this user has not registered.')
 
-      const isMatched = await bcrypt.compare(password, user.password)
-      isMatched
-        ? res.json('successfully log in')
-        : res.json('incorrect email or unregistered email')
+      // Sign a token
+      const token = jwt.sign(req.user, process.env.JWT_SECRET, {
+        expiresIn: '7d',
+      })
+      res.json({
+        token,
+      })
     } catch (error) {
       next(error)
     }
+    // #swagger.tags = ['User']
   },
 
   // user register
