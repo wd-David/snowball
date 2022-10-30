c97fee1abbd54e6923850e3273a020de518e18f5
AdrieneTZ <tznali17@gmail.com>
2022-10-30 15:38:10 +0800

    feat(back-end): add cors

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 const express = require('express')
 const passport = require('./config/passport')
+const cors = require('cors')
 const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require('./swagger_output.json')
 
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use(passport.initialize())
+app.use(cors())
 
 app.use(routes)
 app.listen(port)
